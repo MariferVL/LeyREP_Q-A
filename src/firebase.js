ab9db581f27480b2b70663cc3f9503499b058068
Marifer Villalobos <mfvl.1186@gmail.com>
2023-09-17 18:53:05 -0300

    install and config firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYeaOGPxOQUA8ca2Hgqq0IEnMacMIqtlg",
+  authDomain: "reciclachile-23.firebaseapp.com",
+  projectId: "reciclachile-23",
+  storageBucket: "reciclachile-23.appspot.com",
+  messagingSenderId: "298747882163",
+  appId: "1:298747882163:web:e62f0bf08df1ee1d20dec6"
+};
+
+const app = initializeApp(firebaseConfig);
