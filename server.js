5d3e15b9c994748a2a9e3bb22a3db64914e2c55a
Marifer Villalobos <mfvl.1186@gmail.com>
2023-09-19 17:44:06 -0300

    conect chatbot js files

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
-const express = require('express');
-const path = require('path');
-const dotenv = require('dotenv');
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import {createPrediction} from './controllers/flowise.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static( 'public'));
 
-app.use('/src', express.static(path.join(__dirname, 'src')));
+app.use('/src', express.static('src'));
+
+
+// Use body-parser middleware to parse incoming request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.post("/api/flowise", createPrediction);
+
+
 app.listen(port, () => {
   console.log(`Servidor en ejecución en el puerto ${port}`);
 });
+
+
+
+
+
